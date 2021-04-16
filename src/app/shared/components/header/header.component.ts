0ae7ad03353f31a96be358c9668c5770a9400d9b
Mariana Bastos <maribastossrodrigues@gmail.com>
2021-04-16 20:42:54 -0300

    add skills

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  public menuOpen: boolean = false; 
+  menuOpen: boolean = false; 
 
   constructor() { }
 
